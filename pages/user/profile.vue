f5c0f92b89b47304a2fb982799e52b7f70c74d6f
jonathan-martz <github@jmartz.de>
2025-10-04 21:49:44 +0200

    fix template

--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="bg-white xl:mx-auto mx-3 px-3 py-3 max-w-6xl">
-    User {{ route.query.id == 'Id Missing' }}
+    User {{ route.query.id ?? 'Id Missing' }}
   </section>
 </template>
 
 <script setup lang="ts">
-import useRoute from 'vue-router'
+import { useRoute } from 'vue-router'
 const route = useRoute()
 </script>

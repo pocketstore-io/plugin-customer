5de500fed0386ca2ab525ae3c03ac15490636820
jonathan-martz <github@jmartz.de>
2025-10-04 21:39:49 +0200

    initial commit

---
+++ pages/hello.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="bg-white xl:mx-auto mx-3 px-3 py-3 max-w-6xl">
+    Hallo Welt {{ version}}
+  </section>
+</template>
+
+<script setup lang="ts">
+const version = ref('0.0.1.3');
+</script>
